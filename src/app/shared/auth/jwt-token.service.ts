fb4d190744653a73b912c82f3c1cb061cdb6e699
Vitaliy <prokinvs@gmail.com>
2024-10-20 17:29:10 +0200

    ref(token): token exp service

--- src/app/shared/auth/jwt-token.service.ts
+++ src/app/shared/auth/jwt-token.service.ts
@@ -5,35 +5,15 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class JWTTokenService {
-    decodedToken: { [key: string]: string };
     constructor(private router: Router, private authService: AuthService) { }
 
 
-  private decodeToken() {
-    const token = this.authService.getToken();
-    if (token !== null) {
-      try {
-        this.decodedToken = jwtDecode(token);
-      } catch (error) {
-        this.router.navigate(['/']);
-        return true;
-      }
+    public isTokenExpired(tokent: string): boolean {
+    
+        const token = this.authService.getToken();
+
+        const decodeToken = jwtDecode(token);
+
+        return 1000 * decodeToken.exp - new Date().getTime() < 5000;
     }
-  }
-
-
-  private getExpiryTime() {
-    this.decodeToken();
-    return this.decodedToken ? this.decodedToken.exp : null;
-  }
-
-  public isTokenExpired(): boolean {
-    const expiryTime: number = +this.getExpiryTime();
-    if (expiryTime) {
-      return 1000 * expiryTime - new Date().getTime() < 5000;
-    } else {
-      return false;
-    }
-  }
-
 }
