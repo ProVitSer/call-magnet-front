638c23608019a60284a75befd77a6a54b1a52eee
Vitaliy <prokinvs@gmail.com>
2024-10-22 21:05:27 +0200

    feat(api): back api settings request

--- src/app/small-business/pbx-api/components/settings/service/pbx-api-settings.service.ts
+++ src/app/small-business/pbx-api/components/settings/service/pbx-api-settings.service.ts
@@ -1,67 +1,87 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { Router } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
-import { saveAs } from 'file-saver';
-import { AddPacConnectionData, PbxApiSettings, UpdatePacConnectionData } from "../models/pbx-api-settings.module.ts";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AddPacConnectionData, GetTokenResponse, PbxApiSettings, UpdatePacConnectionData } from '../models/pbx-api-settings.module.ts';
+import { catchError } from 'rxjs/operators';
+import { throwError, firstValueFrom } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
-  })
-
+})
 export class PbxApiSettingsService {
     private readonly apiUrl = `${environment.API_GATEWAY_URL}`;
+
     private readonly pacUrl = `${this.apiUrl}/pac`;
 
-  constructor(    
-    public router: Router, 
-    private http: HttpClient
-   ) {}
+    constructor(
+        public router: Router,
+        private http: HttpClient,
+    ) {}
 
+    public getPacProgramm(selectedOS: string) {
+        return this.http
 
-   public getPacProgramm(selectedOS: string){
-    return this.http.get(`${this.apiUrl}/software-distribution/programm`, {params: { os: selectedOS } , responseType: 'blob' }).subscribe(
-        (response: Blob) => {
-             const downloadURL = window.URL.createObjectURL(response);
-            const link = document.createElement('a');
-            link.href = downloadURL;
-            link.download = `installer-${selectedOS}.exe`; 
-            link.click(); 
-          },
-          (error) => {
-            return throwError(error);
-          }
-      );
-   }
+            .get(`${this.apiUrl}/software-distribution/programm`, { params: { os: selectedOS }, responseType: 'blob' })
 
-   public getPacConfig(){
-        return this.http.get<PbxApiSettings>(`${this.pacUrl}/pac-connector`).pipe(   
-            catchError((error) => {
-                return throwError(error);
-        }));
+            .subscribe(
+                (response: Blob) => {
+                    const downloadURL = window.URL.createObjectURL(response);
+
+                    const link = document.createElement('a');
+
+                    link.href = downloadURL;
+
+                    link.download = `installer-${selectedOS}.exe`;
+
+                    link.click();
+                },
+
+                (error) => {
+                    return throwError(error);
+                },
+            );
     }
 
-    public addPacConfig(data: AddPacConnectionData){
-        return this.http.post<void>(`${this.pacUrl}/pac-connector`, data ).pipe(   
+    public getPacConfig() {
+        return this.http.get<PbxApiSettings>(`${this.pacUrl}/pac-connector`).pipe(
             catchError((error) => {
                 return throwError(error);
-        }));
+            }),
+        );
     }
 
-    public deletePacConnector(){
-        return this.http.delete<void>(`${this.pacUrl}/pac-connector`).pipe(   
+    public addPacConfig(data: AddPacConnectionData) {
+        return this.http.post<void>(`${this.pacUrl}/pac-connector`, data).pipe(
             catchError((error) => {
                 return throwError(error);
-        }));
+            }),
+        );
     }
 
-    public updatePacConnector(data: UpdatePacConnectionData){
-        return this.http.put<void>(`${this.pacUrl}/pac-connector`, data ).pipe(   
+    public deletePacConnector() {
+        return this.http.delete<void>(`${this.pacUrl}/pac-connector`).pipe(
             catchError((error) => {
                 return throwError(error);
-        }));
+            }),
+        );
     }
 
+    public updatePacConnector(data: UpdatePacConnectionData) {
+        return this.http.put<void>(`${this.pacUrl}/pac-connector`, data).pipe(
+            catchError((error) => {
+                return throwError(error);
+            }),
+        );
+    }
+
+    public async getToken() {
+        return firstValueFrom(
+            this.http.get<GetTokenResponse>(`${this.pacUrl}/api-token`).pipe(
+                catchError((error) => {
+                    throw error;
+                }),
+            ),
+        );
+    }
 }
