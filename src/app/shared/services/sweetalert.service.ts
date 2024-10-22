9ba8ac2813ff0c54b15f98908c09d8db813eecb2
Vitaliy <prokinvs@gmail.com>
2024-10-22 21:03:11 +0200

    ref(alert): update by deprecated and lint

--- src/app/shared/services/sweetalert.service.ts
+++ src/app/shared/services/sweetalert.service.ts
@@ -1,77 +1,75 @@
 import swal from 'sweetalert2';
 
 export class SweetalertService {
-    public static errorAlert(title: string, text: string){
+    public static errorAlert(title: string, text: string) {
         swal.fire({
             title,
             text,
-            icon: "error",
+            icon: 'error',
             customClass: {
-              confirmButton: 'btn btn-primary'
+                confirmButton: 'btn btn-primary',
             },
             buttonsStyling: false,
         });
     }
 
-    public static successAlert(title: string, text: string){
+    public static successAlert(title: string, text: string) {
         swal.fire({
             title,
             text,
-            icon: "success",
+            icon: 'success',
             customClass: {
-              confirmButton: 'btn btn-primary'
+                confirmButton: 'btn btn-primary',
             },
             buttonsStyling: false,
-          });
+        });
     }
 
-    public static successAlertWithFunc(title: string, text: string, func: Function){
-      swal.fire({
-          title,
-          text,
-          icon: "success",
-          customClass: {
-            confirmButton: 'btn btn-primary'
-          },
-          buttonsStyling: false,
-        }).then(() => {
-          func()
-        })
-  }
-
-    public static autoCloseSuccessAlert(title: string, text: string, timer: number){
-        let timerInterval
+    public static successAlertWithFunc(title: string, text: string, func: Function) {
         swal.fire({
-          title,
-          text,
-          timer,
-          icon: "success",
-          timerProgressBar: true,
-          customClass: {
-            confirmButton: 'btn btn-primary'
-          },
-          buttonsStyling: false,
-          showConfirmButton: false,
-          onBeforeOpen: () => {
-            swal.showLoading()
-            timerInterval = setInterval(() => {
-              const content = swal.getContent()
-              if (content) {
-                const b = content.querySelector('b')
-                if (b) {
-                  b.textContent = swal.getTimerLeft().toString()
-                }
-              }
-            }, timer)
-          },
-          onClose: () => {
-            clearInterval(timerInterval)
-          }
-        }).then((result) => {
-          if (result.dismiss === swal.DismissReason.timer) {
-          }
-        })
+            title,
+            text,
+            icon: 'success',
+            customClass: {
+                confirmButton: 'btn btn-primary',
+            },
+            buttonsStyling: false,
+        }).then(() => {
+            func();
+        });
     }
 
-
-}  
+    public static autoCloseSuccessAlert(title: string, text: string, timer: number) {
+        let timerInterval;
+        swal.fire({
+            title,
+            text,
+            timer,
+            icon: 'success',
+            timerProgressBar: true,
+            customClass: {
+                confirmButton: 'btn btn-primary',
+            },
+            buttonsStyling: false,
+            showConfirmButton: false,
+            willOpen: () => {
+                swal.showLoading();
+                timerInterval = setInterval(() => {
+                    const content = swal.getContent();
+                    if (content) {
+                        const b = content.querySelector('b');
+                        if (b) {
+                            b.textContent = swal.getTimerLeft().toString();
+                        }
+                    }
+                }, timer);
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            },
+        }).then((result) => {
+            if (result.dismiss === swal.DismissReason.timer) {
+            }
+        });
+    }
+}
