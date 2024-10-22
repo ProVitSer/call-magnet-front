90928e28bc4fd20c6b9e9db8b9ab260a49df542a
Vitaliy <prokinvs@gmail.com>
2024-10-22 21:04:14 +0200

    feat(api): start add api pac online documentations

--- src/app/small-business/pbx-api/components/api/api.component.ts
+++ src/app/small-business/pbx-api/components/api/api.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { environment } from 'environments/environment';
 
 @Component({
-  selector: 'app-api',
-  templateUrl: './api.component.html',
-  styleUrls: ['./api.component.scss'],
+    selector: 'app-api',
+    templateUrl: './api.component.html',
+    styleUrls: ['./api.component.scss'],
 })
-export class ApiComponent implements OnInit, OnDestroy{
-  ngOnInit(): void {
-  }
-  ngOnDestroy(): void {
-  }
-  
+export class ApiComponent {
+    activeTab = 'call';
+    generalFormSubmitted = false;
+    changePasswordFormSubmitted = false;
+    alertVisible = true;
+    generalForm: UntypedFormGroup;
+    apiUrl = `${environment.API_GATEWAY_URL}`;
+    constructor() {}
+
+    reloadPage() {
+        location.reload();
+    }
+
+    setActiveTab(tab) {
+        this.activeTab = tab;
+    }
 }
