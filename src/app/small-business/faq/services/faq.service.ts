26b80e9564b27231a87f68f71b6b4cd928355f26
Vitaliy <prokinvs@gmail.com>
2024-11-07 17:40:12 +0100

    pre test faq

--- src/app/small-business/faq/services/faq.service.ts
+++ src/app/small-business/faq/services/faq.service.ts
@@ -4,21 +4,36 @@ import { FAQMain, FAQ } from '../models/faq.model';
 @Injectable()
 export class FaqService {
     constructor() {}
-    public faqsAnalitic: FAQ[] = [];
+    public faqsAnalitic: FAQ[] = [
+        new FAQ('f1', 'Widgets', ''),
+        new FAQ('f2', 'Графики', ''),
+        new FAQ('f3', 'Статистика по добавочным', ''),
+        new FAQ('f4', 'Cdr', ''),
+        new FAQ('f4', 'Статистика', ''),
+        new FAQ('f4', 'Аналитика звонка', ''),
+        new FAQ('f4', 'Диаграмма вызова', ''),
+        new FAQ('f4', 'Распознавание диалога', ''),
+    ];
     public faqsCRM: FAQ[] = [];
     public faqsApi3CX: FAQ[] = [];
     public faqsKpi: FAQ[] = [];
-    public faqsAutoDial: FAQ[] = [];
+    public faqsSmartRouting: FAQ[] = [];
     public faqsSMS: FAQ[] = [];
     public faqsTelegram: FAQ[] = [];
+    public faqsMissedCall: FAQ[] = [];
+    public faqsTts: FAQ[] = [];
+    public faqsVoip: FAQ[] = [];
 
     public faqmain: FAQMain[] = [
         new FAQMain(0, 'Аналитика', ``, this.faqsAnalitic),
-        new FAQMain(1, 'CRM', ``, this.faqsCRM),
-        new FAQMain(2, '3CX API', ``, this.faqsApi3CX),
-        new FAQMain(3, 'KPI колл-центра', ``, this.faqsKpi),
-        new FAQMain(4, 'Автообзвон', ``, this.faqsAutoDial),
-        new FAQMain(5, 'SMS', ``, this.faqsSMS),
-        new FAQMain(6, 'Telegram', ``, this.faqsTelegram),
+        new FAQMain(1, '3CX API', ``, this.faqsApi3CX),
+        new FAQMain(2, 'Умная маршрутизация', ``, this.faqsSmartRouting),
+        new FAQMain(3, 'Пропущенные вызовы', ``, this.faqsMissedCall),
+        new FAQMain(4, 'KPI колл-центра', ``, this.faqsKpi),
+        new FAQMain(5, 'Синтез речи', ``, this.faqsTts),
+        new FAQMain(6, 'SMS', ``, this.faqsSMS),
+        new FAQMain(7, 'CRM', ``, this.faqsCRM),
+        new FAQMain(8, 'Telegram', ``, this.faqsTelegram),
+        new FAQMain(9, 'VoIP мост', ``, this.faqsVoip),
     ];
 }
